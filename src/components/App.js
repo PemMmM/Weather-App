1e17d83d6db61aaa99626cc26909fe44ec3ec3f8
PemMmM <pembemistikoglu992@gmail.com>
2022-01-23 21:35:06 +0200

    snapshots updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
-  const { city, country } = location;
-  return <LocationDetails city={city} country={country} />;
+function App(props) {
+  const { forecasts, location } = props;
+
+  return (
+    <div className="App">
+      <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
+    </div>
+  );
 }
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      data: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.number,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
